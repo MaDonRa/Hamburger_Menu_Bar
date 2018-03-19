bd32495f82815aa64ce4f1a74f8c9d6abecae5d9
Sakkaphong L <sakkaphong.l@roundtwosolutions.com>
2018-03-19 17:36:27 +0700

    Master

--- silde menu/ViewController.swift
+++ silde menu/ViewController.swift
@@ -10,32 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let Menu : MenuBarDelegate = MenuBarViewController()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.SetupButtonOnNavigationBar()
     }
-    
-    private func SetupButtonOnNavigationBar() {
-        
-        let LeftButton = UIButton(frame: CGRect(x: 0, y: 0, width: 25, height: 25))
-        LeftButton.contentMode = .scaleAspectFit
-        LeftButton.addTarget(self, action: #selector(ViewController.MenuBar), for: .touchUpInside)
-        
-        let MenuBarImage:UIImageView = UIImageView()
-        MenuBarImage.frame = LeftButton.frame
-        MenuBarImage.image = #imageLiteral(resourceName: "Icon_Menu")
-        LeftButton.addSubview(MenuBarImage)
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView:LeftButton)
-    }
-
-    @objc func MenuBar() {
-
-        self.Menu.SetupMenuBar(Controller: self, navigation: self.navigationController)
- 
-    }
-
 }
 
